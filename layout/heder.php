bcc9f06bb0e24b1e026ec26fc047a1ba7a2f3c7f
Huy <huyabcde6@gmail.com>
2023-11-19 20:51:12 +0700

    cat

--- layout/heder.php
+++ layout/heder.php
@@ -10,22 +10,24 @@
     <meta name="author" content="" />
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
 
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./room.php">
-    <link rel="stylesheet" href="./css/owl.carousel.min.css">
-    <link rel="stylesheet" href="./css/aos.css">
-    <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="./css/jquery.timepicker.css">
-    <link rel="stylesheet" href="./css/fancybox.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <!-- /css/bootstrap.min.css -->
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <!-- ./css/owl.carousel.min.css -->
+    <link rel="stylesheet" href="../css/aos.css">
+    <!-- ./css/aos.css -->
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../css/jquery.timepicker.css">
+    <link rel="stylesheet" href="../css/fancybox.min.css">
 
-    <link rel="stylesheet" href="./css/ctsp.css">
+    <link rel="stylesheet" href="../css/ctsp.css">
     
-    <link rel="stylesheet" href="./fonts/ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="./fonts/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="../fonts/fontawesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <!-- Theme Style -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <style>
     #carda {
       border-radius: 4px;
